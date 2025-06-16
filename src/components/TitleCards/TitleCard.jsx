4aaaec3532535977ac7caba0743e12e84020bba5
sarath vp <sarathvp1546@gmail.com>
2025-06-16 22:29:46 +0530

    homepage completed

--- src/components/TitleCards/TitleCard.jsx
+++ src/components/TitleCards/TitleCard.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCard.css'
-const TitleCard = () => {
+import card_data from '../../assets/cards/Cards_data'
+
+const TitleCard = ({title}) => {
+
+  const cardsRef=useRef()
+const handleWheel=(event)=>
+{
+event.preventDefault;
+cardsRef.current.scrollLeft+=event.deltaY;
+}
+  useEffect(()=>
+  {
+  cardsRef.current.addEventListener('wheel',handleWheel)
+  },[])
   return (
-    <div className='titleCard'>TitleCard</div>
+    <div className='titleCard'>
+      <h2>{title?title:'Popular on Netflix'}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {card_data.map((card,i)=>{
+          return <div className='card' key={i}>
+            <img src={card.image} alt='' />
+            <p>{card.name}</p>
+            </div>
+        })}
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
+import hero from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import TitleCard from '../../components/TitleCards/TitleCard'
+import Footer from '../../components/Footer/Footer'
+
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar />
+        <div className="hero">
+            <img src={hero} alt='' className='banner-img'/>
+            <div className="hero-caption">
+                <img src={hero_title} alt="" className='caption-img' />
+                <p>Discovering his ties to a secret ancient order, a young 
+                    man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.
+                </p>
+                <div className="hero-btns">
+                    <button className='btn'><img src={play_icon} alt=''/>Play</button>
+                     <button className='btn dark-btn'><img src={info_icon} alt=''/>More Info</button>
+                </div>
+                     <TitleCard />
+            </div>
+        </div>
+        <div className="more-cards">
+            <TitleCard title='Trending Movies' />
+            <TitleCard title='Action Movies' />
+            <TitleCard title='Top Pick For you'/>
+            <TitleCard title='Block Busters'/>
+
+        </div>
+        <Footer />
     </div>
   )
 }
